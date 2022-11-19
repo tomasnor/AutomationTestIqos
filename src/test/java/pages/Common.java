f96bfd5abca94a8709a099995b52300574526a64
Tomas <norvilat@gmail.com>
2022-11-19 17:04:01 +0200

    Created methods for TestBase.java

--- src/test/java/pages/Common.java
+++ src/test/java/pages/Common.java
@@ -1,4 +1,9 @@
 package pages;
 
+import utils.Driver;
+
 public class Common {
+    public static void close() {
+            Driver.close();
+    }
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,4 +1,17 @@
 package tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.Common;
+
 public class TestBase {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        Common.close();
+    }
 }
