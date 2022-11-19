879864a308ce7eb42e42bf7ebd72bb716584f6d2
Tomas <norvilat@gmail.com>
2022-11-19 17:00:37 +0200

    Created methods for Driver.java

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -1,4 +1,35 @@
 package utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
 public class Driver {
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void setDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(getOptions());
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
+    }
+
+    private static ChromeOptions getOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("window-size=2000,3000");
+        options.addArguments("--force-device-scale-factor=0.85");
+        return options;
+    }
+
+    public static void close() {
+        driver.quit();
+    }
 }
