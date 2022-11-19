8db618b4bc346e0c4e37b3da05c5c12d6cbf39d7
Tomas <norvilat@gmail.com>
2022-11-19 17:09:53 +0200

    Created methods for Common.java

--- src/test/java/pages/Common.java
+++ src/test/java/pages/Common.java
@@ -1,9 +1,43 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import utils.Driver;
 
 public class Common {
-    public static void close() {
-            Driver.close();
+
+    public static void openUrl(String url) {
+        Driver.setDriver();
+        Driver.getDriver().get(url);
     }
+
+    public static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close() {
+        Driver.close();
+    }
+
+    private static WebElement getElement(By locator) {
+        return Driver.getDriver().findElement(locator);
+    }
+
+    public static void sendKeysToElement(By locator, String keys) {
+        getElement(locator).sendKeys(keys);
+    }
+
+    public static void clickElement(By locator) {
+        getElement(locator).click();
+    }
+
+    public static String getTextFromElement(By locator) {
+        return getElement(locator).getText();
+    }
+
+
 }
