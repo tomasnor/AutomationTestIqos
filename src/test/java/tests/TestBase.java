aad9e94abaae5c3bab7ed1d035cdf907e651ca08
Tomas <norvilat@gmail.com>
2022-11-21 14:52:05 +0200

    Implemented TestBase.java with @Listeners.

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,8 +2,11 @@ package tests;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import pages.Common;
+import utils.TestListener;
 
+@Listeners(TestListener.class)
 public class TestBase {
 
     @BeforeMethod
